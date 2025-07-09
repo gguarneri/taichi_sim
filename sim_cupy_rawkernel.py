6b4f7b20ed25a3d7a1fed75b29e8fcc4eb1a1ab1
gguarneri <giovanni@utfpr.edu.br>
2025-07-09 10:21:04 -0300

    Inclusão do simulador CuPy em rawkernel. Ainda não funcional.

---
+++ sim_cupy_rawkernel.py
@@ -0,0 +1,341 @@
+# =======================
+# Importacao de pacotes de uso geral
+# =======================
+import argparse
+from time import time
+import numpy as np
+from sim_support import *
+from sim_support.simulator import Simulator
+
+# ======================
+# Importacao de pacotes especificos para a implementacao do simulador
+# ======================
+import cupy
+import cupyx
+
+
+# -----------------------------------------------------------------------------
+# Aqui deve ser implementado o simulador como uma classe herdada de Simulator
+# -----------------------------------------------------------------------------
+class SimulatorCupyRawkernel(Simulator):
+    def __init__(self, file_config):
+        # Chama do construtor padrao, que le o arquivo de configuracao
+        super().__init__(file_config)
+        
+        # Define o nome do simulador
+        self._name = "CuPy-rawkernel"
+        
+        
+    def implementation(self):
+        # ---------------------------------
+        # Definicao das funcoes de kernel
+        # ---------------------------------
+        # Pressao
+        @cupyx.jit.rawkernel()
+        def pressure_kernel(vx, vy, pressure, kappa,
+                            mdvx_dx, mdvy_dy,
+                            a_x_h, b_x_h, k_x_h,
+                            a_y, b_y, k_y,
+                            coefs, idx_fd,
+                            dt, one_dx, one_dy, p_2,
+                            nx, ny, ord):
+            x, y = cupyx.jit.grid(2)
+            
+            last = ord - 1
+            offset = ord - 1
+            i_dix = -idx_fd[last, 2]
+            i_dfx = nx - idx_fd[last, 0]
+            i_diy = -idx_fd[last, 3]
+            i_dfy = ny - idx_fd[last, 1]
+
+            # Pressure
+            p_2[0] = 0.0
+            if(x >= i_dix and x < i_dfx and y >= i_diy and y < i_dfy):
+                vdvx_dx = 0.0
+                vdvy_dy = 0.0
+                for c in range(0, ord):
+                    vdvx_dx += coefs[c] * (vx[x + idx_fd[c, 0], y] - vx[x + idx_fd[c, 2], y]) * one_dx
+                    vdvy_dy += coefs[c] * (vy[x, y + idx_fd[c, 1]] - vy[x, y + idx_fd[c, 3]]) * one_dy
+
+                mdvx_dx_new = b_x_h[x - offset] * mdvx_dx[x, y] + a_x_h[x - offset] * vdvx_dx
+                mdvy_dy_new = b_y[y - offset] * mdvy_dy[x, y] + a_y[y - offset] * vdvy_dy
+
+                vdvx_dx = vdvx_dx/k_x_h[x - offset] + mdvx_dx_new
+                vdvy_dy = vdvy_dy/k_y[y - offset]  + mdvy_dy_new
+
+                mdvx_dx[x, y] = mdvx_dx_new
+                mdvy_dy[x, y] = mdvy_dy_new
+
+                pressure[x, y] += kappa[x, y]*(vdvx_dx + vdvy_dy) * dt * one_dx * one_dy
+        
+        # Velocidades
+        @cupyx.jit.rawkernel()
+        def velocity_kernel(vx, vy, pressure, rho_grid_vx, rho_grid_vy,
+                            mdpressure_dx, mdpressure_dy,
+                            a_x, b_x, k_x,
+                            a_y_h, b_y_h, k_y_h,
+                            coefs, idx_fd,
+                            dt, one_dx, one_dy,
+                            nx, ny, ord):
+            x, y = cupy.jit.grid(2)
+            
+            last = ord - 1
+            offset = ord - 1
+            i_dix = -idx_fd[last, 3]
+            i_dfx = nx - idx_fd[last, 1]
+            i_diy = -idx_fd[last, 3]
+            i_dfy = ny - idx_fd[last, 1]
+            
+            # Velocidade Vx
+            if(x >= i_dix and x < i_dfx and y >= i_diy and y < i_dfy):
+                dpressure_dx = 0.0
+
+                for c in range(0, ord):
+                    dpressure_dx += coefs[c] * (pressure[x + idx_fd[c, 1], y] - pressure[x + idx_fd[c, 3], y]) * one_dx
+
+                mdpressure_dx_new = b_x[x - offset] * mdpressure_dx[x, y] + a_x[x - offset] * dpressure_dx
+                dpressure_dx = dpressure_dx / k_x[x - offset] + mdpressure_dx_new
+                mdpressure_dx[x, y] = mdpressure_dx_new
+
+                vx[x, y] += dt * (dpressure_dx / rho_grid_vx[x, y])
+            
+            # Velocidade Vy
+            i_dix = -idx_fd[last, 2]
+            i_dfx = nx - idx_fd[last, 0]
+            i_diy = -idx_fd[last, 2]
+            i_dfy = ny - idx_fd[last, 0]
+            
+            if(x >= i_dix and x < i_dfx and y >= i_diy and y < i_dfy):
+                dpressure_dy = 0.0
+
+                for c in range(0, ord):
+                    dpressure_dy += coefs[c] * (pressure[x, y + idx_fd[c, 0]] - pressure[x, y + idx_fd[c, 2]]) * one_dy
+
+                mdpressure_dy_new = b_y_h[y - offset] * mdpressure_dy[x, y] + a_y_h[y - offset] * dpressure_dy
+                dpressure_dy = dpressure_dy / k_y_h[y - offset] + mdpressure_dy_new
+                mdpressure_dy[x, y] = mdpressure_dy_new
+
+                vy[x, y] += dt * (dpressure_dy / rho_grid_vy[x, y])
+               
+        # # Adicao da fonte no campo de pressao
+        @cupyx.jit.rawkernel()
+        def sources_kernel(pressure, source_term, idx_source, it, dt, one_dx, one_dy):
+            x, y = cupyx.jit.grid(2)
+
+            idx_src = idx_source[x, y]
+            if idx_src != -1:
+                pressure[x, y] += source_term[it, idx_src] * dt * one_dx * one_dy
+        
+        # Finalizacao da iteracao
+        @cupyx.jit.rawkernel()
+        def finish_it_kernel(vx, vy, pressure, idx_fd, p_2, nx, ny, ord):
+            x, y = cupyx.jit.grid(2)
+            
+            last = ord - 1
+            i_dix = -idx_fd[last, 2]
+            i_dfx = nx - idx_fd[last, 0]
+            i_diy = -idx_fd[last, 2]
+            i_dfy = ny - idx_fd[last, 0]
+            p_2_old = p_2[0]
+
+            # Aplica as condicoes de Dirichlet
+            if(x < i_dix or x > i_dfx or y < i_diy or y > i_dfy):
+                vx[x, y] = 0.0
+                vy[x, y] = 0.0
+                
+            # Calcula a norma L2 da pressao
+            p_2_new = abs(pressure[x, y])
+            p_2[0] = p_2_old if p_2_old > p_2_new else p_2_new
+            
+        # Armazenamento dos sensores
+        @cupyx.jit.rawkernel()
+        def store_sensors_kernel(vx, vy, pressure,
+                                 sens_vx, sens_vy, sens_pressure,
+                                 offset_sensors, info_rec_pt, delay_rec,
+                                 it):
+            sensor = cupyx.jit.grid(1)
+            
+            pt = offset_sensors[sensor]
+            while info_rec_pt[pt, 2] == sensor:
+                if it >= delay_rec[sensor]:
+                    x = info_rec_pt[pt, 0]
+                    y = info_rec_pt[pt, 1]
+                    sens_vx[it, sensor] += vx[x, y]
+                    sens_vy[it, sensor] += vy[x, y]
+                    sens_pressure[it, sensor] += pressure[x, y]
+                
+                pt += 1
+
+        # ---------------------------------
+        # Implementacao do simulador
+        # ---------------------------------
+        super().implementation()
+        
+        # --------------------------------------------
+        # Aqui comeca o codigo especifico do simulador
+        # --------------------------------------------
+        # Transfere arrays de parametros para a GPU
+        a_x_gpu = cupy.asarray(self._a_x.flatten())
+        b_x_gpu = cupy.asarray(self._b_x.flatten())
+        k_x_gpu = cupy.asarray(self._k_x.flatten())
+        a_x_half_gpu = cupy.asarray(self._a_x_half.flatten())
+        b_x_half_gpu = cupy.asarray(self._b_x_half.flatten())
+        k_x_half_gpu = cupy.asarray(self._k_x_half.flatten())
+        a_y_gpu = cupy.asarray(self._a_y.flatten())
+        b_y_gpu = cupy.asarray(self._b_y.flatten())
+        k_y_gpu = cupy.asarray(self._k_y.flatten())
+        a_y_half_gpu = cupy.asarray(self._a_y_half.flatten())
+        b_y_half_gpu = cupy.asarray(self._b_y_half.flatten())
+        k_y_half_gpu = cupy.asarray(self._k_y_half.flatten())
+        
+        rho_grid_vx_gpu = cupy.asarray(self._rho_grid_vx)
+        rho_grid_vy_gpu = cupy.asarray(self._rho_grid_vy)        
+        coefs_gpu = cupy.asarray(self._coefs)
+
+        # Arrays para as variaveis de memoria do calculo
+        memory_dvx_dx_gpu = cupy.asarray(np.zeros((self._nx, self._ny), dtype=flt32))
+        memory_dvy_dy_gpu = cupy.asarray(np.zeros((self._nx, self._ny), dtype=flt32))
+        memory_dpressure_dx_gpu = cupy.asarray(np.zeros((self._nx, self._ny), dtype=flt32))
+        memory_dpressure_dy_gpu = cupy.asarray(np.zeros((self._nx, self._ny), dtype=flt32))
+
+        # Arrays dos campos de velocidade e pressoes
+        vx_gpu = cupy.asarray(np.zeros((self._nx, self._ny), dtype=flt32))
+        vy_gpu = cupy.asarray(np.zeros((self._nx, self._ny), dtype=flt32))
+        pressure_gpu = cupy.asarray(np.zeros((self._nx, self._ny), dtype=flt32))
+        pressure_l2_norm_gpu = cupy.asarray(np.zeros(1, dtype=flt32))
+        
+        # Arrays para os sensores
+        sens_vx_gpu = cupy.asarray(np.zeros((self._n_steps, self._n_rec), dtype=flt32))
+        sens_vy_gpu = cupy.asarray(np.zeros((self._n_steps, self._n_rec), dtype=flt32))
+        sens_pressure_gpu = cupy.asarray(np.zeros((self._n_steps, self._n_rec), dtype=flt32))
+        offset_sensors_gpu = cupy.asarray(self._offset_sensors)
+        info_rec_pt_gpu = cupy.asarray(self._info_rec_pt)
+        delay_rec_gpu = cupy.asarray(self._delay_recv)
+
+        # Calculo dos indices para o staggered grid
+        ord = self._coefs.shape[0]
+        idx_fd = np.array([[c + 1, c, -c, -c - 1] for c in range(ord)], dtype=int32)
+        idx_fd_gpu = cupy.asarray(idx_fd)
+
+        # Definicao dos limites para a plotagem dos campos
+        v_max = 100.0
+        v_min = - v_max
+        ix_min = self._roi.get_ix_min()
+        ix_max = self._roi.get_ix_max()
+        iy_min = self._roi.get_iz_min()
+        iy_max = self._roi.get_iz_max()
+
+        # Inicializa os mapas dos parametros de Lame
+        kappa_gpu = cupy.asarray(self._rho_grid_vx * self._cp_grid_vx * self._cp_grid_vx)
+
+        # Acrescenta eixo se source_term for array unidimensional
+        if self._n_pto_src == 1:
+            self._source_term = self._source_term[:, np.newaxis]
+        source_term_gpu = cupy.asarray(self._source_term)
+        idx_src_gpu = cupy.asarray(self._pos_sources)
+        
+        # Define os tamanhos dos blocos e dos grids para os kernels
+        self._block_size_x = np.gcd(self._nx, 16)
+        self._block_size_y = np.gcd(self._ny, 16)
+        block_size = (self._block_size_x, self._block_size_y)
+        grid_fields = ((self._nx + block_size[0] - 1) // block_size[0],
+                       (self._ny + block_size[1] - 1) // block_size[1])
+        sens_blk_sz = np.gcd(self._idx_rec_offset, 32)
+        grid_sens = (self._idx_rec_offset + sens_blk_sz - 1) // sens_blk_sz
+        
+        # Laco de tempo para execucao da simulacao
+        t_gpu = time()
+        for it in range(1, self._n_steps + 1):
+            # Calculo da pressao
+            pressure_kernel[grid_fields, block_size](vx_gpu, vy_gpu, pressure_gpu, kappa_gpu,
+                                                     memory_dvx_dx_gpu, memory_dvy_dy_gpu,
+                                                     a_x_half_gpu, b_x_half_gpu, k_x_half_gpu,
+                                                     a_y_gpu, b_y_gpu, k_y_gpu,
+                                                     coefs_gpu, idx_fd_gpu,
+                                                     self._dt, self._one_dx, self._one_dy, pressure_l2_norm_gpu,
+                                                     self._nx, self._ny, ord)
+            
+            # Calculo das velocidades
+            velocity_kernel[grid_fields, block_size](vx_gpu, vy_gpu, pressure_gpu, rho_grid_vx_gpu, rho_grid_vy_gpu,
+                                                     memory_dpressure_dx_gpu, memory_dpressure_dy_gpu,
+                                                     a_x_gpu, b_x_gpu, k_x_gpu,
+                                                     a_y_half_gpu, b_y_half_gpu, k_y_half_gpu,
+                                                     coefs_gpu, idx_fd_gpu,
+                                                     self._dt, self._one_dx, self._one_dy,
+                                                     self._nx, self._ny, ord)
+            
+            # Adicao da fonte no campo de pressao
+            sources_kernel[grid_fields, block_size](pressure_gpu, source_term_gpu, idx_src_gpu,
+                                                    it, self._dt, self._one_dx, self._one_dy)
+            
+            # Aplica as condicoes de Dirichlet
+            finish_it_kernel[grid_fields, block_size](vx_gpu, vy_gpu, pressure_gpu, idx_fd_gpu,
+                                                      pressure_l2_norm_gpu, self._nx, self._ny, ord)
+
+            # Store seismograms
+            store_sensors_kernel[grid_sens, sens_blk_sz](vx_gpu, vy_gpu, pressure_gpu,
+                                   sens_vx_gpu, sens_vy_gpu, sens_pressure_gpu,
+                                   offset_sensors_gpu, info_rec_pt_gpu, delay_rec_gpu, it)
+
+            psn2 = pressure_l2_norm_gpu.get()[0]
+            if (it % self._it_display) == 0 or it == 5:
+                if self._show_debug:
+                    print(f'Time step # {it} out of {self._n_steps}')
+                    print(f'Max pressure = {psn2}')
+
+                if self._show_anim:
+                    # self._windows_gpu[0].imv.setImage(vx_gpu[ix_min:ix_max, iy_min:iy_max].get(), levels=[v_min, v_max])
+                    # self._windows_gpu[1].imv.setImage(vy_gpu[ix_min:ix_max, iy_min:iy_max].get(), levels=[v_min, v_max])
+                    self._windows_gpu[0].imv.setImage(pressure_gpu[ix_min:ix_max, iy_min:iy_max].get(), levels=[v_min, v_max])
+                    self._app.processEvents()
+
+            # Verifica a estabilidade da simulacao
+            if psn2 > STABILITY_THRESHOLD:
+                raise StabilityError("Simulacao tornando-se instavel", psn2)
+                
+        sim_time = time() - t_gpu
+
+        # Pega os resultados da simulacao
+        vx = vx_gpu.get()
+        vy = vy_gpu.get()
+        pressure = pressure_gpu.get()
+        sens_vx = sens_vx_gpu.get()
+        sens_vy = sens_vy_gpu.get()
+        sens_pressure = sens_pressure_gpu.get()
+
+        # --------------------------------------------
+        # A funcao de implementacao do simulador deve retornar
+        # um dicionario com as seguintes chaves:
+        #   - "vx": campo de velocidade no eixo x
+        #   - "vygpu": campo de velocidade no eixo y
+        #   - "pressuregpu": campo de pressao
+        #   - "sens_vx": sinais de vx nos sensores
+        #   - "sens_vy": sinais de vy nos sensores
+        #   - "sens_pressure": sinais da pressao nos sensores
+        #   - "gpu_str": string de identificacao da GPU utilizada na simulacao
+        #   - "sim_time": tempo da simulacao, medido com a funcao time()
+        # --------------------------------------------
+        return {"vx": vx, "vy": vy, "pressure": pressure,
+                "sens_vx": sens_vx, "sens_vy": sens_vy, "sens_pressure": sens_pressure,
+                "gpu_str": cupy.cuda.runtime.getDeviceProperties(0)["name"].decode(), "sim_time": sim_time}
+
+
+# ----------------------------------------------------------
+# Avaliacao dos parametros na linha de comando
+# ----------------------------------------------------------
+parser = argparse.ArgumentParser()
+parser.add_argument('-c', '--config', help='Configuration file', default='config.json')
+args = parser.parse_args()
+
+# Cria a instancia do simulador
+sim_instance = SimulatorCupyRawkernel(args.config)
+
+# Executa simulacao
+try:
+    sim_instance.run()
+
+except KeyError as key:
+    print(f"Chave {key} nao encontrada no arquivo de configuracao.")
+    
+except ValueError as value:
+    print(value)
