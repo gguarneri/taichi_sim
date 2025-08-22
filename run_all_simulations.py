bef7b7030f6d06e0ff10d5a01e8d1ce1137f9298
gguarneri <giovanni@utfpr.edu.br>
2025-08-22 14:15:00 -0300

    Teste para não rodar os simuladores em CPU. Inclusão de tempo entre execução dos simuladores, para não dar erro em simuladores CuPY implementados com "rawkernel".

--- run_all_simulations.py
+++ run_all_simulations.py
@@ -2,6 +2,7 @@ import glob
 import subprocess
 import sys
 import os
+import time
 
 env = os.environ.copy()
 env["PYTHONPATH"] = os.pathsep.join(sys.path)
@@ -12,18 +13,20 @@ print(files)
 config_file_path = os.path.join('.', 'ensaios', 'ponto', 'ponto_sem_plots.json')
 
 for file in files:
-    print("Running: ", file)
-    resultado = subprocess.run(
-        [
-            sys.executable,
-            file,
-            "-c",
-            config_file_path
-        ],
-        capture_output=True,
-        text=True,
-        env=env
-    )
+    if file.find("cpu") == -1:
+        print("Running: ", file)
+        resultado = subprocess.run(
+            [
+                sys.executable,
+                file,
+                "-c",
+                config_file_path
+            ],
+            capture_output=True,
+            text=True,
+            env=env
+        )
 
-    print("STDOUT:\n", resultado.stdout)
-    print("STDERR:\n", resultado.stderr)
+        print("STDOUT:\n", resultado.stdout)
+        print("STDERR:\n", resultado.stderr)
+        time.sleep(1)
